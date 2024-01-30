906ee5b78bbe1f2f8c7f67fc6027fe548a128d3b
Brian Tipton <briantiptondev@gmail.com>
2024-01-30 11:08:55 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 sudo apt update && sudo apt upgrade -y
-sudo apt install screen htop git -y
+sudo apt install tmux screen htop git -y
 git clone https://github.com/BrianTipton1/CS490ShellScripts.git ~/CS490ShellScripts
 yes | "${SHELL}" <(curl -L micro.mamba.pm/install.sh)
 echo "alias conda=micromamba" >> ~/.bashrc
